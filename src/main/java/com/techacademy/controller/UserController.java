3cc969f8f44605b4e5b58612f2635752cb321d9c
atsushimachida <atsmcd1029@gmail.com>
2023-03-05 12:39:23 +0900

    Lesson17 chapter10まで

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -1,14 +1,16 @@
 package com.techacademy.controller;
 
-import java.util.Set; // 追加
+import java.util.Set; 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult; 
+import org.springframework.validation.annotation.Validated; 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam; // 追加
+import org.springframework.web.bind.annotation.RequestParam; 
 
 import com.techacademy.entity.User;
 import com.techacademy.service.UserService;
@@ -37,10 +39,15 @@ public class UserController {
         // User登録画面に遷移
         return "user/register";
     }
+   
 
     /** User登録処理 */
     @PostMapping("/register")
-    public String postRegister(User user) {
+    public String postRegister(@Validated User user,BindingResult res,Model model) {
+        if(res.hasErrors()) {
+            //エラーあり
+            return getRegister(user);
+        }
         // User登録
         service.saveUser(user);
         // 一覧画面にリダイレクト
